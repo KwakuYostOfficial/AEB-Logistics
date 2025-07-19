27d34dba9a2db01ab7964f9db2268f9fa1a3614a
KwakuYostOfficial <jordanpinto280@gmail.com>
2025-07-19 00:17:41 +0000

    made changes to some files

--- app/Script/Home.js
+++ app/Script/Home.js
@@ -5,8 +5,12 @@ const mobMenu = document.querySelector('.header__navigation-menu-mob');
 menu.addEventListener('click', () => {
     if (mobMenu.classList.contains('show-menu')) {
         mobMenu.classList.remove('show-menu');
+        mobMenu.classList.remove('fade-in');
+        mobMenu.classList.add('fade-out');
     } else {
         mobMenu.classList.add('show-menu');
+        mobMenu.classList.remove('fade-out');
+        mobMenu.classList.add('fade-in');
     }
 });
 
