b16500656e438258707d5f3d8e1017b1aa26dfde
KwakuYostOfficial <jordanpinto280@gmail.com>
2025-08-13 00:08:48 +0000

    Animated the humberger menu

--- app/Script/Pages/Home/home.js
+++ app/Script/Pages/Home/home.js
@@ -1,1 +1,19 @@
 import '../../General/general.js';
+
+/* NAVIGATION MENU BAR SECTION */
+
+const homeMenu =  document.querySelector('.header__menu');
+const mobMenu = document.querySelector('.header__navigation-menu-mob');
+
+
+homeMenu.addEventListener('click', () => {
+    if (mobMenu.classList.contains('show-menu')) {
+        mobMenu.classList.remove('show-menu');
+        mobMenu.classList.remove('fade-in');
+        mobMenu.classList.add('fade-out');
+    } else {
+        mobMenu.classList.add('show-menu');
+        mobMenu.classList.remove('fade-out');
+        mobMenu.classList.add('fade-in');
+    }
+});
--- app/Script/Pages/Tracking/tracking.js
+++ app/Script/Pages/Tracking/tracking.js
@@ -1,1 +1,15 @@
 import '../../General/general.js';
+
+
+/* || NAVIGATION MENU BAR SECTION */
+const menu = document.querySelector('.header__humberger-menu');
+const menuBar = document.querySelector('.menu-bar');
+
+menu.addEventListener('click', () => {
+    if (menuBar.classList.contains('clicked')) {
+        menuBar.classList.remove('clicked');
+
+    } else {
+        menuBar.classList.add('clicked');
+    }
+});
