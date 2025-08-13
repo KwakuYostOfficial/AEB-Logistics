87f12833e700824141c768752edb71d6378674ef
KwakuYostOfficial <jordanpinto280@gmail.com>
2025-08-13 03:36:25 +0000

    Made the static state of the menu overlay

--- app/Script/Pages/Tracking/tracking.js
+++ app/Script/Pages/Tracking/tracking.js
@@ -4,10 +4,13 @@ import '../../General/general.js';
 /* || NAVIGATION MENU BAR SECTION */
 const menu = document.querySelector('.header__humberger-menu');
 const menuBar = document.querySelector('.menu-bar');
+const overlay = document.querySelector('.overlay');
+
 
 menu.addEventListener('click', () => {
     if (menuBar.classList.contains('clicked')) {
-        menuBar.classList.remove('clicked');
+        setTimeout(()=> {menuBar.classList.remove('clicked')}, 100);
+        
 
     } else {
         menuBar.classList.add('clicked');
