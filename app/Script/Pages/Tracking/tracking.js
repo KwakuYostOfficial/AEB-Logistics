788d773ee1b80b5855e53acdc2d2e38b2bfdac31
KwakuYostOfficial <jordanpinto280@gmail.com>
2025-08-14 17:19:54 +0000

    Made the animated state of the navigation menu

--- app/Script/Pages/Tracking/tracking.js
+++ app/Script/Pages/Tracking/tracking.js
@@ -2,17 +2,26 @@ import '../../General/general.js';
 
 
 /* || NAVIGATION MENU BAR SECTION */
-const menu = document.querySelector('.header__humberger-menu');
+const menuIcon = document.querySelector('.header__humberger-menu');
 const menuBar = document.querySelector('.menu-bar');
 const overlay = document.querySelector('.overlay');
+const menu = document.querySelector('.header__nav');
 
 
-menu.addEventListener('click', () => {
+menuIcon.addEventListener('click', () => {
     if (menuBar.classList.contains('clicked')) {
-        setTimeout(()=> {menuBar.classList.remove('clicked')}, 100);
-        
+        menuBar.classList.remove('clicked');
+        overlay.classList.remove('overlay-animation-down');
+        overlay.classList.add('overlay-animation-up');
+        menu.classList.remove('nav-animation-down');
+        menu.classList.add('nav-animation.up');
 
     } else {
         menuBar.classList.add('clicked');
+        overlay.classList.add('overlay-animation-down');
+        overlay.classList.remove('overlay-animation-up');
+        menu.classList.remove('nav-animation-up');
+        menu.classList.add('nav-animation-down');
+
     }
 });
